6e71576b54c2054f7fb706761a9b993823ecbaee
Thomas DoyleEngler <tpdoyle87@gmail.com>
2023-12-04 13:54:09 -0500

    reset routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
     get '/offers', to: 'offers#index'
   end
 
-  mount_devise_token_auth_for 'User', at: 'auth', defaults: {format: 'json'}, controllers: {
-    registrations: 'custom_registrations'
-  }
+  mount_devise_token_auth_for 'User', at: 'auth', defaults: {format: 'json'}
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
